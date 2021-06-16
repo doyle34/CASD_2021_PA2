eaaf863ad79ca0b24bc05c26882e82d999ecd6da
doyle34 <parkdi0409@gmail.com>
2021-06-17 06:16:28 +0900

    fuck code

--- main.cpp
+++ main.cpp
@@ -3,66 +3,63 @@
 #endif
 
 // #include <memory>
-#include "CASD_SurfaceReconstruction.hpp"
 
 #define Cos(th) cos(acos(-1.0)/180.0*(th))
 #define Sin(th) sin(acos(-1.0)/180.0*(th))
 
 #include <cstdarg>
-#include <GL/glut.h>
-
+// #include <GL/glut.h>
 #include <iostream>
+#include "casd_pa2.h"
+
 using namespace std;
-// using namespace HashColon::CAGD;
+using namespace CASD_PA2;
 
-void ReadFile()
-{
-
-	/* read file here */	
-}
-
-void PrepareModel()
-{
-	/* build topology model & reconstruct surface */
-}
-
+// void DrawShip();
 // DrawShape is a callback function for glDisplayFunc()
 // This function should be in form (void)func()
-void DrawShip(/*do not add parameter here*/)
-{
-	/* draw ship hull here with 
-	* glBegin(GL_Triangles),
-	* glVertex3f, glVertex3d, 
-	* glEnd()
-	*/
-
-	/* sample code !!*/
-	glBegin(GL_TRIANGLES);
-	glColor3f(1.0, 0.0, 0.75);
-
-	glVertex3f(6.6f, 0.0f, 4.0f);
-	glVertex3f(8.25f, 0.265f, 5.0f);
-	glVertex3f(6.6f, 0.0f, 5.0f);
-
-	glVertex3f(8.25f, 0.265f, 5.0f);
-	glVertex3f(6.6f, 0.0f, 4.0f);
-	glVertex3f(8.25f, 0.4665f, 4.0f);
-
-	glVertex3f(6.6f, 0.0f, 5.0f);
-	glVertex3f(8.25f, 0.265f, 5.0f);
-	glVertex3f(8.25f, 0.0f, 6.0f);
-
-	glVertex3d(-1, 1, 0);
-	glVertex3d(1, 1, 0);
-	glVertex3d(0, -1, 0);
-
-	glEnd();
-}
+void DrawShip();
 
 int main(int argc, char* argv[])
 {
-	// ReadFile();
 	auto offsettabledata = ReadOffSetTable("offset_table_rev.txt");
-	PrepareModel();
+	auto model = BuildTopology(offsettabledata);
+	ReconstructSurface(model);
+	GL_Object::ship_model = model;
+	// DrawAllSurface(model);
 	Visualization(argc, argv);
+}
+
+void DrawShip(/*do not add parameter here*/)
+{
+	/* draw ship hull here with
+	* glBegin(GL_Triangles),
+	* glVertex3f, glVertex3d,
+	* glEnd()
+	*/
+	DrawAllSurface(GL_Object::ship_model);
+	// DrawVertexes(GL_Object::ship_model);
+	// DrawCP(GL_Object::ship_model);
+
+	/* sample code !!*/
+	//glBegin(GL_TRIANGLES);
+	//glColor3f(1.0, 0.0, 0.75);
+
+	//glVertex3f(6.6f, 0.0f, 4.0f);
+	//glVertex3f(8.25f, 0.265f, 5.0f);
+	//glVertex3f(6.6f, 0.0f, 5.0f);
+
+	//glVertex3f(8.25f, 0.265f, 5.0f);
+	//glVertex3f(6.6f, 0.0f, 4.0f);
+	//glVertex3f(8.25f, 0.4665f, 4.0f);
+
+	//glVertex3f(6.6f, 0.0f, 5.0f);
+	//glVertex3f(8.25f, 0.265f, 5.0f);
+	//glVertex3f(8.25f, 0.0f, 6.0f);
+
+	//glVertex3d(-1, 1, 0);
+	//glVertex3d(1, 1, 0);
+	//glVertex3d(0, -1, 0);
+
+	//glEnd();
 }
